4169ae8bed287fe78a978e405b28a1749adc11d8
Samet Yazak <yazaksamet@gmail.com>
2017-12-27 22:31:02 +0300

    sliding window notebooks

--- code/r/student_s_t.R
+++ code/r/student_s_t.R
@@ -1,4 +1,4 @@
-x <- seq(-5, 5, length=1000)
+x <- seq(-5, 5, length=10)
 standnorm <- dnorm(x, sd= 1, mean=0)
 student <- dt(x, df=4)
 plot(x,standnorm,type="l",ylab="Density",col="red",lty=1,lwd=3,main="Standard normal (red) and Student's t (blue)")

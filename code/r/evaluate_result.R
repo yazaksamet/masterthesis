1c83681433677b94378b021ecf0f1bd2ded4f23d
Samet Yazak <yazaksamet@gmail.com>
2018-05-09 03:07:47 +0300

    thesis file

--- code/r/evaluate_result.R
+++ code/r/evaluate_result.R
@@ -1,8 +1,9 @@
 setwd("C:/Users/sametyazak/Desktop/ynwa/bau/2017 - Thesis/code/r/")
-fullResultSet = read.csv(file="kalman_result_full.csv", header=TRUE, sep=",")
+fullResultSet = read.csv(file="window_20171227.csv", header=TRUE, sep=",")
 fullResultSet = cbind(fullResultSet, 
       recall = (fullResultSet$tp / (fullResultSet$tp+fullResultSet$fn)),
       precision = fullResultSet$tp / (fullResultSet$tp+fullResultSet$fp)
 )
 
 fullResultSet = cbind(fullResultSet, f1_score = 2 * (fullResultSet$precision*fullResultSet$recall) / (fullResultSet$precision+fullResultSet$recall))
+write.csv2(fullResultSet, "result_view.csv")
